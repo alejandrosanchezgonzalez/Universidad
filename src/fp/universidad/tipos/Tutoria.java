0576b3094236c8737c0ecda419b8bae25d120477
Alejandro Sánchez González <alejandrosanchezgonza18@gmail.com>
2026-02-18 12:43:03 +0100

    tutoria casi terminada

---
+++ src/fp/universidad/tipos/Tutoria.java
@@ -0,0 +1,44 @@
+package fp.universidad.tipos;
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+
+public record Tutoria(
+		DayOfWeek dia_de_la_semana,
+		LocalTime hora_inicio,
+		LocalTime hora_fin
+		) {
+	//dia, hora inicio y hora fin
+	public Tutoria(DayOfWeek dia_de_la_semana, LocalTime hora_inicio,LocalTime hora_fin){
+	
+	//dia, hora inicio y duracion
+
+	public Tutoria(DayOfWeek dia_de_la_semana, LocalTime hora_inicio, int duracion_min){
+		this.dia_de_la_semana=dia_de_la_semana;
+		this.hora_inicio=hora_inicio;
+		this.hora_inicio=hora_inicio.plusMinutes(duracion_min);
+	}
+		
+	}
+	
+	public long duracion() {
+		return ChronoUnit.MINUTES.between(hora_inicio,hora_fin);
+	}
+	
+	public String toString() {
+		return letraDia(dia_de_la_semana) +" "+ hora_inicio + "-" + hora_fin;
+	}
+	
+	private static String letraDia(DayOfWeek dia_de_la_semana) {
+		return switch (dia_de_la_semana) {
+		case MONDAY -> "L";
+        case TUESDAY -> "M";
+        case WEDNESDAY -> "X";
+        case THURSDAY -> "J";
+        case FRIDAY -> "V";
+        default -> "";
+    };
+    
+	}
+}
+
