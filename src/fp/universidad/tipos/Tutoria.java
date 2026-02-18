30f49f282f82d0da527dadbdfaa91e3c0765ea3f
Alejandro Sánchez González <alejandrosanchezgonza18@gmail.com>
2026-02-18 12:49:55 +0100

    tutoria terminado

--- src/fp/universidad/tipos/Tutoria.java
+++ src/fp/universidad/tipos/Tutoria.java
@@ -4,22 +4,20 @@ import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
 
 public record Tutoria(
-		DayOfWeek dia_de_la_semana,
-		LocalTime hora_inicio,
-		LocalTime hora_fin
-		) {
-	//dia, hora inicio y hora fin
-	public Tutoria(DayOfWeek dia_de_la_semana, LocalTime hora_inicio,LocalTime hora_fin){
-	
-	//dia, hora inicio y duracion
+        DayOfWeek dia_de_la_semana,
+        LocalTime hora_inicio,
+        LocalTime hora_fin
+) {
 
-	public Tutoria(DayOfWeek dia_de_la_semana, LocalTime hora_inicio, int duracion_min){
-		this.dia_de_la_semana=dia_de_la_semana;
-		this.hora_inicio=hora_inicio;
-		this.hora_inicio=hora_inicio.plusMinutes(duracion_min);
-	}
-		
-	}
+
+//  día + hora_inicio + duración
+public Tutoria(DayOfWeek dia_de_la_semana,
+            LocalTime hora_inicio,
+            int duracion_min) {
+ this(dia_de_la_semana,
+      hora_inicio,
+      hora_inicio.plusMinutes(duracion_min));
+}
 	
 	public long duracion() {
 		return ChronoUnit.MINUTES.between(hora_inicio,hora_fin);
